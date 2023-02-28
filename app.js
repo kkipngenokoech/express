c83b4f615f2aa402c39c68917971ee96ce7b9ce2
kkipngenokoech <kkipngenokoech22@gmail.com>
2023-02-28 21:57:39 +0300

    working on the templating

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ var path = require('path')
 var bodyParser = require('body-parser')
 var nodemailer = require('nodemailer')
 
+
 var app = express()
+app.set('view engine', 'pug')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false}))
 
+app.set('views',path.join(__dirname, 'views'))
+
 app.get('/', function(req, res){
     console.log('Hello Multiverse, I am Obsessed with Quantum computing')
-    res.send('Hello multiverse, I am obsessed with cloud computing')
+    // res.send('Hello multiverse, I am obsessed with cloud computing')
+    res.render('index')
 })
 
 
